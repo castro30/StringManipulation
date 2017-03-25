0a270534d629f19bfe0ecf9ede98bf27c5a97aa8
castro30 <castro30@yahoo.com>
2017-03-25 13:17:12 -0500

    Corrected IfStatements

---
+++ StringManipulation.cs/StringManipulation/StringManipulation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringManipulation
+{
+    class StringManipulation
+    {
+        static void Main(string[] args)
+        {
+            // Write a program which accepts two strings. 
+            // The program should do the following: 
+            Console.WriteLine("Please inter a sentence");
+            string sentence = Console.ReadLine();
+
+
+            // print and display the total number of occurrences of a or A
+            // replace every occurrence of „a‟ or „A‟ with @ 
+            // convert both the strings to upper case 
+            // concatenate the two strings and display the result 
+            // declare one variable of type StringBuilder 
+            // perform operations like Append(), Insert(), Remove(), Replace().
+        }
+    }
+}
