de1a5c8d8efe84c9bcceb9f2c520c4a9c94b7868
castro30 <castro30@yahoo.com>
2017-03-25 23:32:42 -0500

    Counting the characters only

--- StringManipulation.cs/StringManipulation/StringManipulation.cs
+++ StringManipulation.cs/StringManipulation/StringManipulation.cs
@@ -15,13 +15,50 @@ namespace StringManipulation
             Console.WriteLine("Please inter a sentence");
             string sentence = Console.ReadLine();
 
+            for (int counter = 0; counter < sentence.Length; counter++)
+            {
+                int anOccurancea = 0;
+
+                foreach (char newMine in sentence[counter].ToString())
+                {
+
+                    anOccurancea = anOccurancea + 1;
+
+                    Console.WriteLine(anOccurancea);
+                }
+
+                    Console.WriteLine(sentence[counter].ToString());
+            }
+            
+
+
+            sentence += " welcome";
+            sentence += "to ";
+            sentence += "Dot Net";
+
+            Console.WriteLine(sentence);
+
+            
+
+
 
             // print and display the total number of occurrences of a or A
+            char theA;
+            
+
+            
+
+
             // replace every occurrence of „a‟ or „A‟ with @ 
             // convert both the strings to upper case 
             // concatenate the two strings and display the result 
             // declare one variable of type StringBuilder 
             // perform operations like Append(), Insert(), Remove(), Replace().
+
+            StringBuilder MyStringBuilder = new StringBuilder("Hello World!");
+            MyStringBuilder.Append(" What a beautiful day.");
+            Console.WriteLine(MyStringBuilder);
+            Console.ReadKey();
         }
     }
 }
